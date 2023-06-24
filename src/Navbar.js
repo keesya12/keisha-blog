32363513c371411e959ec01359e0c1c12f4fe66f
Keisha Avila <kissya422@gmail.com>
2023-06-24 21:21:47 +0800

    added homepage

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="Navbar">
+            <h1>Keisha's Blog</h1>
+            <div className="Navlinks">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+                <a href="/">Contact</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
